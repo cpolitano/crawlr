a5324745ba12f927270859ab03b7c81e392ac6fe
Clare Hutchings <clareh@socialtables.com>
2015-12-31 12:01:14 -0500

    successful call to google places

--- src/server/routes/places.js
+++ src/server/routes/places.js
@@ -1,24 +1,37 @@
-import fetch from "isomorphic-fetch";
+var request = require("request");
 
-const getNearbySpots = (lat, lon) => {
-  	let uri = [
-  		"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=",
-  		lat,
-  		lon,
-  		"&radius=500",
-  		"&types=food",
-  		"&key=",
-  		process.env.GOOGLE_KEY
-  	].join("");
+module.exports = function(router) {
 
-	fetch(uri, {
-		method: "get",
-		headers: {
-			"Content-Type": "application/json",
-			Accept: "application/json"
+	router.get("/places", function() {
+		try {
+			var uri = [
+		  		"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=",
+		  		"38.9135905",
+		  		",",
+		  		"-77.0091621",
+		  		"&radius=500",
+		  		"&types=bar|restaurant|cafe",
+		  		"&opennow",
+		  		"&key=",
+		  		process.env.GOOGLE_KEY
+		  	].join("");
+
+		  	var res = request(uri, function (error, response, body) {
+			  	if (!error && response.statusCode === 200) {
+			  		console.log(body);
+			  		return body;
+				} else {
+					console.log(error);
+				}
+			});
+
+			this.body = res;
+
+		} catch (err) {
+			console.log(err);
+			this.status = 500;
+			return;
 		}
-	})
-	.then(resp => 
-		console.log(resp)
-	);
-}
+	});
+
+};
--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -2,5 +2,6 @@ var express = require("express");
 var router = express.Router();
 
 require("./index")(router);
+require("./places")(router);
 
 module.exports = router;
